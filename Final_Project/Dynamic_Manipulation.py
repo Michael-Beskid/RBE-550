d16cd91b5be6a05af5bf5f65eee5efdf28b4efcc
Michael Beskid <73857255+Michael-Beskid@users.noreply.github.com>
2023-11-27 09:57:07 -0500

    Implemented dynamics for 2-link robot

--- Final_Project/Dynamic_Manipulation.py
+++ Final_Project/Dynamic_Manipulation.py
@@ -1,12 +1,23 @@
 from Robot_Util import Robot
 import numpy as np
+import time
 
 
 ## Manipulator Configuration
-#robot = Robot([10, 10, 10], [np.pi/6, np.pi/6, -np.pi/4]) # Generic 3-link manipulator
-robot = Robot([10, 10], [0, 0]) # Generic 2-link manipulator
+#robot = Robot([10, 10, 10], [np.pi/6, np.pi/6, -np.pi/4], [1, 1, 1]) # Generic 3-link manipulator
+robot = Robot([0.5, 0.5], [0, 0], [1, 1]) # Generic 2-link manipulator
 
 def main():
+    # Show robot
     robot.visualize()
+
+    for x in range(15):
+        # Simulate robot
+        joint_angles, joint_velocities = robot.fwd_dyn([0, 0], 100)
+        print(joint_angles)
+        print(joint_velocities)
+
+        # Update robot display
+        robot.visualize()
 
 main()
--- Final_Project/Robot_Util.py
+++ Final_Project/Robot_Util.py
@@ -1,38 +1,84 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-## Forward Dynamics
-def skew(w):
-    return np.array([
-        [0, -w[2], w[1]],
-        [w[2], 0, -w[0]],
-        [-w[1], w[0], 0]
-    ])
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
 
 
 class Robot:
-    def __init__(self, link_lengths, joint_angles):
+    def __init__(self, link_lengths, joint_angles, link_masses):
 
         self.link_lengths = link_lengths
         self.num_links = len(link_lengths)
 
-        self.joint_angles = joint_angles
-        self.joint_velocities = np.zeros(self.num_links)
-        self.joint_accelerations = np.zeros(self.num_links)
+        self.joint_angles = np.zeros([self.num_links,1])
+        self.joint_angles[0] = joint_angles[0]
+        self.joint_angles[1] = joint_angles[1]
+        self.joint_velocities = np.zeros([self.num_links,1])
+        self.joint_accelerations = np.zeros([self.num_links,1])
 
-        self.M = 0  # Need to construct mass matrix
+        self.link_masses = link_masses
+
+        self.M = np.zeros([2, 2]) # Compute mass matrix
+        self.calc_mass_matrix()
+
+        self.C = np.zeros([2, 1]) # Compute Coriolis matrix
+        self.calc_coriolis_matrix()
+
+        self.G = np.zeros([2, 1]) # Compute gravity matrix
+        self.calc_gravity_matrix()
+
+        # Set plot limits to max extension length of manipulator
+        self.plot_lim = 0
+        for length in self.link_lengths:
+            self.plot_lim += length
+        
+
+    # Calculate the mass matrix
+    def calc_mass_matrix(self):
+        self.M[0][0] = self.link_masses[0]*self.link_lengths[0]**2 + self.link_masses[1]*(self.link_lengths[0]**2 + 2*self.link_lengths[0]*self.link_lengths[1]*np.cos(self.joint_angles[1] + self.link_lengths[1]**2))
+        self.M[0][1] = self.link_masses[1]*(self.link_lengths[0]*self.link_lengths[1]*np.cos(self.joint_angles[1] + self.link_lengths[1]**2))
+        self.M[1][0] = self.link_masses[1]*(self.link_lengths[0]*self.link_lengths[1]*np.cos(self.joint_angles[1] + self.link_lengths[1]**2))
+        self.M[1][1] = self.link_masses[1]*self.link_lengths[1]**2
 
 
-    def fwd_dyn(self, joint_torques):
-        pass
+    # Calculate the Coriolis matrix
+    def calc_coriolis_matrix(self):
+        self.C[0] = -self.link_masses[1]*self.link_lengths[0]*self.link_lengths[1]*np.sin(self.joint_angles[1])*(2*self.joint_velocities[0]*self.joint_velocities[1] + self.joint_velocities[1]**2)
+        self.C[1] = self.link_masses[1]*self.link_lengths[0]*self.link_lengths[1]*self.joint_velocities[0]**2*np.sin(self.joint_angles[1])
 
 
-    ## Visalize Manipulator
+    # Calculate the gravity matrix
+    def calc_gravity_matrix(self):
+        g = 9.81
+        self.G[0] = (self.link_masses[0] + self.link_masses[1])*self.link_lengths[0]*g*np.cos(self.joint_angles[0]) + self.link_masses[1]*g*self.link_lengths[1]*np.cos(self.joint_angles[0] + self.joint_angles[1])
+        self.G[1] = self.link_masses[1]*g*self.link_lengths[1]*np.cos(self.joint_angles[0] + self.joint_angles[1])
+
+    
+    # Compute joint accelerations from joint positions, joint velocities, and torques
+    def calc_joint_accel(self, joint_torques):
+        self.calc_mass_matrix()
+        self.calc_coriolis_matrix()
+        self.calc_gravity_matrix()
+        self.joint_accelerations = np.matmul(np.linalg.inv(self.M), joint_torques - self.C - self.G)
+    
+
+    def fwd_dyn(self, input_vector, time_steps):
+        time_step = 0.001
+        joint_torques = np.zeros([2,1])
+        joint_torques[0] = input_vector[0]
+        joint_torques[1] = input_vector[1]
+        for x in range(time_steps):
+            self.calc_joint_accel(joint_torques)
+            self.joint_angles = self.joint_angles + self.joint_velocities * time_step
+            self.joint_velocities = self.joint_velocities + self.joint_accelerations * time_step
+        return self.joint_angles, self.joint_velocities
+
+
     def visualize(self):
-        fig, ax = plt.subplots()
-        ax.set_aspect('equal')
-
+        # Clear the plot
+        ax.clear()
+        
         # Starting point
         points = np.array([[0, 0]])
 
@@ -50,48 +96,14 @@ class Robot:
             # Plot the manipulator link
             ax.plot([points[i, 0], points[i + 1, 0]], [points[i, 1], points[i + 1, 1]], 'bo-')
 
-        # Set plot limits to max extension length of manipulator
-        plot_lim = 0
-        for length in self.link_lengths:
-            plot_lim += length
-
         # Plot settings
         ax.set_title('Planar Serial Manipulator')
         ax.set_xlabel('X-axis')
         ax.set_ylabel('Y-axis')
-        ax.set_xlim(0,plot_lim)
-        ax.set_ylim(0,plot_lim)
+        ax.set_xlim(-self.plot_lim, self.plot_lim)
+        ax.set_ylim(-self.plot_lim, self.plot_lim)
         plt.grid()
-        plt.show()
+        plt.show(block=False)
         # plt.savefig('manipulator_plot.png')
 
-
-# def fk(links, angles):
-#     num_links = len(links)
-#     T = np.identity(4)
-#     identity = np.identity(3)
-
-#     for i in range(num_links):
-#         theta = angles[i]
-#         link_length = links[i]
-
-#         # Define joint twist for revolute joint
-#         joint_twist = np.array([0, 0, link_length])
-
-#         # Define joint axis (Z-axis for simplicity)
-#         joint_axis = np.array([0, 0, 1])
-
-#         # Construct exponential twist matrix
-#         w_skew = skew(joint_twist)
-#         R = np.identity(3) + np.sin(theta) * w_skew + (1 - np.cos(theta)) * (w_skew @ w_skew) 
-#         p = (identity * theta + (1 - np.cos(theta)) * w_skew + (theta - np.sin(theta)) * (w_skew @ w_skew)) @ joint_axis
-
-#         # Construct transformation matrix
-#         Ti = np.identity(4)
-#         Ti[:3, :3] = R
-#         Ti[:3, 3] = p
-
-#         # Update total transformation matrix
-#         T = np.dot(T, Ti)
-
-#     return T
+        plt.pause(0.1)
